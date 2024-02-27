b996cc232197c080eeb57b874eecf1b26561a4cb
unknown <minwidth49@gmail.com>
2024-02-27 06:22:06 +0000

    removed gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import Navbar from "./components/Navbar"
-import WelcomeText from "./components/WelcomeText"
-import Values from "./components/Values"
-import WhatWeDo from "./components/WhatWeDo"
-import Services from "./components/Services"
-import WhatWeDeliver from "./components/WhatWeDeliver"
-import WhatOthersSay from "./components/WhatOthersSay"
-import IndustriesWeServe from "./components/IndustriesWeServe"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar";
+import WelcomeText from "./components/WelcomeText";
+import Values from "./components/Values";
+import WhatWeDo from "./components/WhatWeDo";
+import Services from "./components/Services";
+import WhatWeDeliver from "./components/WhatWeDeliver";
+import WhatOthersSay from "./components/WhatOthersSay";
+import IndustriesWeServe from "./components/IndustriesWeServe";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <main className={`w-full md:h-screen h-[130vh]   bg-cover main`}>
-      <Navbar/>
-      <WelcomeText/>
-      <Values/>
-    </main>
-    <WhatWeDo/>
-    <Services/>
-    <WhatWeDeliver/>
-    <WhatOthersSay/>  
-    <IndustriesWeServe/>
-    <Footer/>
+      <main className={`w-full md:h-screen h-[130vh]   bg-cover main`}>
+        <Navbar />
+        <WelcomeText />
+        <Values />
+      </main>
+      <WhatWeDo />
+      <Services />
+      <WhatWeDeliver />
+      <WhatOthersSay />
+      <IndustriesWeServe />
+      <Footer />
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
